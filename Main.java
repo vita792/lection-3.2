bfc4bfdab8d52767bcaf65b773b1cedfcf8df54c
vita792 <59779169+vita792@users.noreply.github.com>
2020-06-25 13:56:49 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,40 +1,22 @@
 package com.company.lesson2;
 
-        import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        int[] userNumbers = new int[3];
-        System.out.println("Enter three numbers: ");
-        Scanner numb = new Scanner(System.in);
-        for (int i =0; i < userNumbers.length; i++) {
-            userNumbers[i] = numb.nextInt();
-        }
-        if        (userNumbers[0]%2 != 0 && userNumbers[1]%2 != 0  && userNumbers[2]%2 != 0) {
-            System.out.println("Your summ is: " + (userNumbers[0] + userNumbers[1] + userNumbers[2]));
-
-        } else if (userNumbers[0]%2 == 0 && userNumbers[1]%2 != 0  && userNumbers[2]%2 != 0) {
-            System.out.println("Your summ is: " + (userNumbers[1] + userNumbers[2]));
-
-        } else if (userNumbers[0]%2 == 0 && userNumbers[1]%2 == 0 && userNumbers[2]%2 != 0) {
-            System.out.println("Your summ is: " + (userNumbers[2]));
-
-        } else if (userNumbers[0]%2 == 0 && userNumbers[1]%2 != 0 && userNumbers[2]%2 == 0) {
-            System.out.println("Your summ is: " + (userNumbers[1]));
-
-        } else if (userNumbers[0]%2 != 0 && userNumbers[1]%2 == 0 && userNumbers[2]%2 != 0) {
-            System.out.println("Your summ is: " + (userNumbers[0] + userNumbers[2]));
-
-        } else if (userNumbers[0]%2 != 0 && userNumbers[1]%2 == 0 && userNumbers[2]%2 == 0) {
-            System.out.println("Your summ is: " + (userNumbers[0]));
-
-        } else if (userNumbers[0]%2 != 0 && userNumbers[1]%2 != 0  && userNumbers[2]%2 == 0) {
-            System.out.println("Your summ is: " + (userNumbers[0] + userNumbers[1]));
-
-        } else {
-            System.out.println("There are no odd numbers!" );
-        }
-    }
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	   int summOdd = 0;
+	   Scanner arrSize = new Scanner (System.in);
+	   System.out.print("Enter size for array: ");
+	   int size = arrSize.nextInt();
+	   int[] arrOfNumb = new int[size];
+	   System.out.println("Enter array`s numbers: ");
+	   for (int i = 0; i < arrOfNumb.length; i++){
+	       arrOfNumb[i] = arrSize.nextInt();
+	       
+	       if (arrOfNumb[i]%2 != 0) {
+	           summOdd += arrOfNumb[i];
+	       }
+	   }
+	  System.out.println("Array`s numbers (summOdd): " + summOdd);
+	}    
 }
